81dd2b66d6ae1ae389975d893cafb539f8d0f5ab
Ali Haidar <ecommerceedittor@gmail.com>
2026-05-22 10:30:00 +0500

    add react-tweet/styling

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,8 @@
-// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-// import { SportFilterProvider } from "./context/SportFilter";
-
-// const queryClient = new QueryClient();
-
-// <QueryClientProvider client={queryClient}>
-//   <SportFilterProvider>
-//     <App />
-//   </SportFilterProvider>
-// </QueryClientProvider>
-
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-
+import "react-tweet/styles";
 import { BrowserRouter } from "react-router-dom";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
