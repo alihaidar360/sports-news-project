260b3f2e2f4589e786e7eb45a992258ec84d8cdb
Ali Haidar <ecommerceedittor@gmail.com>
2026-05-29 18:15:11 +0500

    manual sitemap fix

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import sitemap from "vite-plugin-sitemap";
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    sitemap({
-      hostname: "https://sports-news-project.vercel.app",
-    }),
+    
   ],
 });
