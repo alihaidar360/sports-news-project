5352f9e2f2bc2ff6ce0291e60287a00fd4354e40
Ali Haidar <ecommerceedittor@gmail.com>
2026-06-18 01:58:49 +0500

    twets section

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { SpeedInsights } from "@vercel/speed-insights/next"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SportFilterProvider } from "./context/SportFilter";
 
